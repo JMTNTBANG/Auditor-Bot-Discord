e191fbda3d881b03c5038b19e44bc4e6ff9133c3
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2023-11-28 00:29:30 -0600

    Added Bot Functionality

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import Dependencies
+import os
+try:
+    import discord
+    import dotenv
+except ImportError:
+    print("Please Run \"poetry install\" before running the bot")
+    exit(1)
+
+# Load .env
+dotenv.load_dotenv()
+token = os.getenv("BOT_TOKEN")
+if token is None:
+    print("Please Add Discord Bot Token in .env")
+    exit(1)
+
+# Bot Variables
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = discord.Client(intents=intents)
+cmd_tree = discord.app_commands.CommandTree(bot)
+del intents
+
+
+# Bot Code
+@bot.event
+async def on_ready():
+    print(f"Signed in as: {bot.user}")
+
+# Sign In
+if __name__ == '__main__':
+    bot.run(token)
